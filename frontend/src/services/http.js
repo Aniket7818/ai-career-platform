2ee4c4cfe9c159bdadeb7af3fa4ba110df49a9bf
Aniket <aniketyadav5705@gmail.com>
2026-06-07 16:35:39 +0530

    Connect frontend to Render API

--- frontend/src/services/http.js
+++ frontend/src/services/http.js
@@ -1,9 +1,12 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: '/api/v1',
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
   withCredentials: true,
-  headers: { 'Content-Type': 'application/json', Accept: 'application/json' }
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
+  }
 })
 
 export default http
